8c743630d33eaf1c76e7a6d4b95c0d58d22ef1b4
TeunStout <teunstout@hotmail.com>
2020-03-21 20:35:46 +0100

    formatted

--- Level1Task1/app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
+++ Level1Task1/app/src/main/java/com/example/level1task1/HigherLowerActivity.kt
@@ -6,58 +6,51 @@ import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class HigherLowerActivity : AppCompatActivity() {
-    private var currentThrow: Int = 1       // current throw
-    private var lastThrow: Int = 1          // last throw
-    // images in a array
-    private val img = intArrayOf(R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6)
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+    private val img = intArrayOf(
+        R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
-        updateUI()                                                          // call update
-        // Listeners
-        btnLower.setOnClickListener{onLowerClick()}
-        btnEquals.setOnClickListener{onEqualClick()}
-        btnHigher.setOnClickListener{onHigherClick()}
+        updateUI()
+
+        btnLower.setOnClickListener { onLowerClick() }
+        btnEquals.setOnClickListener { onEqualClick() }
+        btnHigher.setOnClickListener { onHigherClick() }
     }
 
     fun updateUI() {
         lastThrow = currentThrow                                            // last throw is this throw
         currentThrow = (1..6).random()                                      // roll again
         dice.setImageResource(img[currentThrow - 1])                        // set image. (-1 for right index)
-        lastThrowText.text = getString(R.string.textLastThrow, lastThrow)   // set the text of the last throw. 2nd variable is parameter
+        lastThrowText.text = getString(R.string.textLastThrow, lastThrow)   // set the text of the last throw
     }
-
-    // functions for buttons
 
     fun onHigherClick() {
         updateUI()
         toastMessage(currentThrow > lastThrow)
-//        if (currentThrow > lastThrow) onAnswerCorrect() else onAnswerIncorrect()
     }
 
-    fun onLowerClick(){
+    fun onLowerClick() {
         updateUI()
         toastMessage(currentThrow < lastThrow)
-//        if (currentThrow < lastThrow) onAnswerCorrect() else onAnswerIncorrect()
     }
 
     fun onEqualClick() {
         updateUI()
         toastMessage(currentThrow == lastThrow)
-//        if (currentThrow == lastThrow) onAnswerCorrect() else onAnswerIncorrect()
     }
 
-//    fun onAnswerCorrect(){
-//        Toast.makeText(this, R.string.correct , Toast.LENGTH_SHORT).show()
-//    }
-//
-//    fun onAnswerIncorrect() {
-//        Toast.makeText(this, R.string.incorrect , Toast.LENGTH_SHORT).show()
-//    }
-
-    fun toastMessage(awnserGood: Boolean){
-        val message = if(awnserGood) R.string.correct else R.string.incorrect
-        Toast.makeText(this, message , Toast.LENGTH_SHORT).show()
+    fun toastMessage(awnserGood: Boolean) {
+        val message = if (awnserGood) R.string.correct else R.string.incorrect
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
